6442da9df8b15b2e50f778d4ad154855c406af2b
Egor <82820305+Ryagich@users.noreply.github.com>
2026-03-28 17:15:53 +0500

    ChestLifetimeScope

---
+++ Assets/Scripts/Container/ChestLifetimeScope.cs
@@ -0,0 +1,17 @@
+﻿using Interactable;
+using VContainer;
+using VContainer.Unity;
+
+namespace Container
+{
+    public class ChestLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            var interactable = gameObject.AddComponent<Interactable.Interactable>();
+            interactable.InteractionMode = InteractionMode.Manual;
+            
+            builder.RegisterInstance(interactable);
+        }
+    }
+}
