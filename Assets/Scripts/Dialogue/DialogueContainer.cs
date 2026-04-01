423a5fd3dae97769676ac8f2fdc7b87b37a09003
Egor <82820305+Ryagich@users.noreply.github.com>
2026-04-01 09:57:18 +0500

    Fix DialogueContainer

--- Assets/Scripts/Dialogue/DialogueContainer.cs
+++ Assets/Scripts/Dialogue/DialogueContainer.cs
@@ -6,9 +6,9 @@ namespace Dialogue
     public class DialogueContainer : MonoBehaviour
     {
         [field: SerializeField] public ScrollRect DialogueScroll { get; private set; }
-        [field: SerializeField] public ScrollRect DialogueContent { get; private set; }
+        [field: SerializeField] public RectTransform DialogueContent { get; private set; }
         [field: SerializeField] public ScrollRect AnswerScroll { get; private set; }
-        [field: SerializeField] public ScrollRect AnswerContent { get; private set; }
+        [field: SerializeField] public RectTransform AnswerContent { get; private set; }
         [field: SerializeField] public Button TradeButton { get; private set; }
     }
 }
